b2750b432342b1487e8446883ba55e248c6a5bd8
kmorganfield <kennedymorganfield@gmail.com>
2019-06-09 15:23:56 -0500

    Initial Commit

---
+++ AviatrixApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AviatrixApp
+//
+//  Created by Kennedy Morganfield on 6/9/19.
+//  Copyright © 2019 Kennedy Morganfield. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
